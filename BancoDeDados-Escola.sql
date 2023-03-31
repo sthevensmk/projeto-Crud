74d22b1a511e151ed91b802dc76c60631473633c
sthevensmk <128431254+sthevensmk@users.noreply.github.com>
2023-03-31 19:26:35 -0300

    Add files via upload

---
+++ BancoDeDados-Escola.sql
@@ -0,0 +1,38 @@
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           10.1.33-MariaDB - mariadb.org binary distribution
+-- OS do Servidor:               Win32
+-- HeidiSQL Versão:              9.5.0.5196
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Copiando estrutura do banco de dados para escola
+CREATE DATABASE IF NOT EXISTS `escola` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `escola`;
+
+-- Copiando estrutura para tabela escola.aluno
+CREATE TABLE IF NOT EXISTS `aluno` (
+  `codigo` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(250) DEFAULT NULL,
+  `cidade` varchar(250) DEFAULT NULL,
+  `sexo` varchar(1) DEFAULT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+
+-- Copiando dados para a tabela escola.aluno: ~3 rows (aproximadamente)
+/*!40000 ALTER TABLE `aluno` DISABLE KEYS */;
+INSERT INTO `aluno` (`codigo`, `nome`, `cidade`, `sexo`) VALUES
+	(10, 'Fabiano', 'Curitiba', 'm'),
+	(11, 'Fabio', 'Florianópolis', 'm'),
+	(15, 'Fred', 'Registro', 'm');
+/*!40000 ALTER TABLE `aluno` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
